94bae70f1755d7acfba097c7854fd0080361cc77
zxc <zhaoxuanchao@kids-creative.com.cn>
2020-08-31 20:56:13 +0800

    update build

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 npm install
 git submodule init
 git submodule update
+# https://www.haomwei.com/technology/maupassant-hexo.html
+npm install hexo-renderer-pug --save
+npm install hexo-renderer-sass --save #the great word
+
