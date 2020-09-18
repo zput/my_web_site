5d38f9280093abcff6380e628fba16c5a2575bb3
jxy <jiangxiaoyu@kids-creative.com.cn>
2020-09-18 18:48:36 +0800

    ok

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 npm install
 git submodule init
 git submodule update
+npm install hexo-cli -g
 # https://www.haomwei.com/technology/maupassant-hexo.html
 npm install hexo-renderer-pug --save
 npm install hexo-renderer-sass --save #the great word
